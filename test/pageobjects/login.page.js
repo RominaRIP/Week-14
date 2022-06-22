4c68c54a8f39d7735f2f842b4407e79ad5a1c016
Romina Pogliani <romina_pogliani14@hotmail.com>
2022-06-22 02:04:01 -0300

    BaSP-QA: login test automation and fixed errors

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,42 +1,29 @@
+class LoginPage {
+    //Getters
+    get inputUsername () { return $('#user-name') }
+    get inputPassword () { return $('#password') }
+    get btnLogin () { return $('#login-button') }
+    get errorMsgContainer () { return $('.error-message-container.error')}
+    get robotImg () { return $('.bot_column') }
+    get logoImg () { return $('.login_logo') }
 
-
-import Page from './page';
-
-/**
- * sub page containing specific selectors and methods for a specific page
- */
-class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    get inputUsername () {
-        return $('#username');
-    }
-
-    get inputPassword () {
-        return $('#password');
-    }
-
-    get btnSubmit () {
-        return $('button[type="submit"]');
-    }
-
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
-    async login (username, password) {
+    //Setters
+    async setUsername(username) {
         await this.inputUsername.setValue(username);
+    };
+    async setPassword(password) {
         await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
-    }
+    };
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
+    //Methods
+    async login (username, password) {
+        await this.setUsername(username);
+        await this.setPassword(password);
+        await this.btnLogin.click();
+    };
+
     open () {
-        return super.open('login');
+        return browser.url('http://www.saucedemo.com/');
     }
 }
-
 export default new LoginPage();
---
+++ test/specs/login.spec.js
@@ -0,0 +1,69 @@
+const LoginPage = require('../pageobjects/login.page');
+
+describe ('Login page testing', () => {
+  beforeAll('Open browser', () => {
+    browser.url('https://www.saucedemo.com/')
+  });
+
+  describe('Username input test', () => {
+    it('Empty username field should display an error message'), async () => {
+        await LoginPage.login('', 'secret_sauce');
+        await expect(LoginPage.errorMsgContainer).toHaveText('Epic sadface: Username is required')
+    };
+    it('Invalid username field should display an error message'), async () => {
+        await LoginPage.open();
+        await LoginPage.login('invalidusertest', 'secret_sauce');
+        await expect(LoginPage.errorMsgContainer).toHaveText('Epic sadface: Username and password do not match any user in this service')
+    };
+    it('Locked out username input should display an error message'), async () => {
+        await LoginPage.open();
+        await LoginPage.login('locked_out_user', 'secret_sauce');
+        await expect(LoginPage.errorMsgContainer).toHaveText('Epic sadface: Sorry, this user has been locked out.')
+    };
+    it('Problem username input should redirect to the products page with errors in images'), async () => {
+        await LoginPage.open();
+        await LoginPage.login('problem_user', 'secret_sauce');
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+        const imgProblemSrc = await $('#item_4_img_link > img:nth-child(1)').getAttribute('src');
+        await expect(imgProblemSrc).toBe('/static/media/sl-404.168b1cce.jpg');
+    };
+    it('Login with performance glitch username'), async () => {
+      await LoginPage.open();
+      await LoginPage.login('performance_glitch_user', 'secret_sauce');
+      await browser.setTimeout({ 'pageLoad': 10000 });
+      await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+    }
+  });
+
+  describe('Password input test', () => {
+    it('Empty password field should display an error message'), async () => {
+      await LoginPage.open();
+      await LoginPage.login('standard_user', '');
+      await expect(LoginPage.errorMsgContainer).toHaveText('Epic sadface: Password is required')
+    };
+    it('Invalid password field should display an error message'), async () => {
+      await LoginPage.open();
+      await LoginPage.login('standard_user', 'invalidpasstest');
+      await expect(LoginPage.errorMsgContainer).toHaveText('Epic sadface: Username and password do not match any user in this service')
+    }
+  });
+
+  describe('Login with valid credentials', () => {
+    it('It should login and redirect to the products page'), async () => {
+        await LoginPage.open();
+        await LoginPage.login('standard_user', 'secret_sauce');
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+    }
+  });
+
+  describe('Elements display test in login page', () => {
+    it('Image of a robot should be displayed next to the input fields'), async () => {
+      await LoginPage.open();
+      await expect(LoginPage.robotImg).toExist()
+    };
+    it('Logo image should be displayed in the top of the page'), async () => {
+      await LoginPage.open();
+      await expect(LoginPage.logoImg).toExist()
+    }
+  })
+})
